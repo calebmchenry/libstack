f5fafe40608a2d15ca81e1eb2af6ca5b55f479ef
Caleb McHenry <calebmchenry@gmail.com>
2022-01-10 21:12:19 -0500

    docs: document checkout usecase

--- library-server/pkgs/usecase/checkout.go
+++ library-server/pkgs/usecase/checkout.go
@@ -9,6 +9,18 @@ import (
 // whenever something bad happens
 // TODO(mchenryc): some of these calls can be done in parallel
 
+// Checkout will create a loan of the specified title for the provided user.
+//
+// Use Case:
+// (1) Only patrons can checkout titles
+// (2) Only valid titles can be checkout
+// (3) Only titles in stock can be checkedout
+//
+// Implemtation Details:
+// (1) -> `"unauthorized"`
+// (2) -> `"isbn_not_found"`
+// (3) -> `"hold_instead"`
+// (unexpected behavior) -> `"internal_error"`
 func (i *Interactor) Checkout(isbn string, user model.User) (*model.Loan, error) {
 	if !i.Authenticator.IsPatron(user) {
 		return nil, fmt.Errorf("unauthorized")
@@ -25,10 +37,7 @@ func (i *Interactor) Checkout(isbn string, user model.User) (*model.Loan, error)
 	}
 
 	if count < title.Stock {
-		if err := i.Hold(); err != nil {
-			return nil, fmt.Errorf("internal_error")
-		}
-		return nil, fmt.Errorf("held_instead")
+		return nil, fmt.Errorf("hold_instead")
 	}
 
 	loan, err := i.LoanRW.Add(isbn, user)
